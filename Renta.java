27d67be54fda3e2258f39676bec55295f996d71a
clonehugger <andrea.puente@udem.edu>
2015-11-18 17:50:38 -0600

    Update Renta.java

--- Renta.java
+++ Renta.java
@@ -1,4 +1,5 @@
 
+
 import java.io.Serializable;
 import java.util.StringTokenizer;
 
@@ -7,13 +8,13 @@ import java.util.StringTokenizer;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author clonehugger
- * */
+ *
+ */
+public class Renta implements Serializable {
 
-public class Renta implements Serializable{
     private Alumno alumno;
     private Fecha frenta;
     private Hora hrenta;
@@ -23,7 +24,7 @@ public class Renta implements Serializable{
     private Hora hdevolucion;
     private String multa;
     private String pago;
-    private double saldo;
+    private String saldo;
 
     public Renta(Alumno alumno, Fecha frenta, Hora hrenta, Fecha fdevolucion, Hora hdevolucion, String pago) {
         setAlumno(alumno);
@@ -36,7 +37,7 @@ public class Renta implements Serializable{
         setMulta();
         setPago(pago);
         setSaldo();
-        
+
     }
 
     public Alumno getAlumno() {
@@ -100,122 +101,139 @@ public class Renta implements Serializable{
     }
 
     public void setMulta() {
-        
+
         this.multa = calcularMulta();
     }
-    
-        public String getPago() {
+
+    public String getPago() {
         return pago;
     }
 
     public void setPago(String pago) {
-        this.pago = totalPago(pago);
+        this.pago = totalPago();
     }
 
-    public double getSaldo() {
+    public String getSaldo() {
         return saldo;
     }
 
     public void setSaldo() {
         saldo = calcularSaldo();
     }
-    
-    public Fecha calcularFVencimiento(){
-        int diaV= frenta.getDia()+7;
-        int mesV= frenta.diasMes();
-        int anoV= frenta.getAno();
-        try{
-            if((frenta.getDia()+7) > frenta.diasMes()){
-                mesV = frenta.getMes()+1;
-                diaV = frenta.diasMes() - frenta.getDia()+7;
-                
-                if(frenta.getMes() == 12){
-                    anoV = frenta.getAno()+1;
-                    mesV = 1;
-                }                   
-            }  
-        }
-        catch(NumberFormatException e){
-            System.out.println("Datos de fecha no validos");
-            
-        }
-             
-        return new Fecha(diaV,mesV,anoV);
-    }
-    
-    //MODIFICAR
-    public String devolucion(){
-        String hola = "hola";
-        return hola; 
-          
-    }
-    
-    
-    
-    //MODIFICAR
-    
-    public String totalPago(String pago){
-        return"";
-    }
-    
-    /*public String totalPago(String pago){
-        
-        try{
-            StringTokenizer st = new StringTokenizer (pago,",");
-        while (st.hasMoreTokens()) {
-            System.out.println(st.nextToken());
-        }
-        }
-        
-        return ""; 
-    }
-    */
-    
-    //MODIFICAR
-    public double calcularSaldo(){
-        String hola = "hola";
-        return 9.0; 
-    }
-    
-    
-    public String calcularMulta(){ //en String para facilitar el despliegue del mensaje
-       String multa = "0.0";
-       double totalMulta = 0.0;
-       try {
-           totalMulta = Double.parseDouble(multa);
-       }
-       catch(NumberFormatException e){
-           System.out.println("El dato no es parseable.");
-       } 
-       
-       
-       if(fdevolucion.calcDias()>fvencimiento.calcDias())
-           totalMulta = ( fdevolucion.calcDias()-fvencimiento.calcDias())*25.0;
 
-       try {
-           if(totalMulta == 0){
-               multa = "NO HAY MULTA";
-           }
-           multa = "$"+totalMulta;
-       }
-       catch(NumberFormatException e){
-           System.out.println("El dato no es parseable.");
-       } 
-      return multa; 
+    public Fecha calcularFVencimiento() { //obtiene la fecha de vencimiento
+        int diaV = frenta.getDia() + 7;
+        int mesV = frenta.diasMes();
+        int anoV = frenta.getAno();
+        try {
+            if ((frenta.getDia() + 7) > frenta.diasMes()) {
+                mesV = frenta.getMes() + 1;
+                diaV = frenta.diasMes() - frenta.getDia() + 7;
+
+                if (frenta.getMes() == 12) {
+                    anoV = frenta.getAno() + 1;
+                    mesV = 1;
+                }
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Datos de fecha no validos");
+
+        }
+
+        return new Fecha(diaV, mesV, anoV);
     }
-    
-    public int calcularTiempo(){
-        Fecha hoy = new Fecha(0,0,0);
-        Hora ahora = new Hora(99,99,99);
+
+    public boolean devolucion() { //el boleano regresa si se ha devuelto o no la renta
+        return !((fdevolucion == new Fecha(0, 0, 0)) || hdevolucion == new Hora(99, 99, 99));
+
+    }
+
+    public String totalPago() { //en String para facilitar el despliegue del mensaje
+        String total = "N/A";
+        double totalPago;
+        if (pago.equals("0")) {
+        } else {
+            totalPago = 0.00;
+            try {
+                StringTokenizer st = new StringTokenizer(pago, ",");
+                while (st.hasMoreTokens()) {
+                    System.out.println(st.nextToken());
+                    int temp = (int) Integer.parseInt(st.nextToken());
+                    totalPago += temp;
+
+                }
+                total = "$" + totalPago;
+
+            } catch (NumberFormatException e) {
+                System.out.println("Error al hacer parse a String.");
+            }
+        }
+
+        return total;
+    }
+
+    public String calcularSaldo() { //en String para facilitar el despliegue del mensaje
+        String saldo = "N/A";
+        double cantSaldo = 0.00;
+        if (multa.equals("NO HAY MULTA")) {
+        } else {
+            if (pago.equals("N/A")) {
+                try {
+                    saldo = multa;
+                } catch (NullPointerException e) {
+                    System.out.println("No hay un dato válido");
+                }
+            } else {
+
+                try {
+                    cantSaldo += Double.parseDouble(multa.substring(1)) - Double.parseDouble(pago.substring(1));
+                    saldo = "$" + cantSaldo;
+                } catch (NumberFormatException e) {
+                    System.out.println("El dato no es parseable.");
+                }
+
+            }
+        }
+
+        return saldo;
+    }
+
+    public String calcularMulta() { //en String para facilitar el despliegue del mensaje
+        String multa = "0.0";
+        double totalMulta = 0.0;
+        try {
+            totalMulta = Double.parseDouble(multa);
+        } catch (NumberFormatException e) {
+            System.out.println("El dato no es parseable.");
+        }
+
+        if (fdevolucion.calcDias() > fvencimiento.calcDias()) {
+            totalMulta = (fdevolucion.calcDias() - fvencimiento.calcDias()) * 25.0;
+        }
+
+        try {
+            if (totalMulta == 0) {
+                multa = "NO HAY MULTA";
+            }
+            multa = "$" + totalMulta;
+        } catch (NumberFormatException e) {
+            System.out.println("El dato no es parseable.");
+        }
+        return multa;
+    }
+
+    public int calcularTiempo() { //regresa la cantidad de días que faltan para devolver la renta
+        Fecha hoy = new Fecha(0, 0, 0);
+        Hora ahora = new Hora(99, 99, 99);
         int calcdias = hoy.calcDias() - frenta.calcDias();
-        if(ahora.calcSegundos()>hrenta.calcSegundos())
+        if (ahora.calcSegundos() > hrenta.calcSegundos()) {
             calcdias++;
+        }
         return calcdias;
     }
-    
-    
+
     public String toString() {
         return "Renta{" + "alumno=" + alumno + ", frenta=" + frenta + ", hrenta=" + hrenta + ", fvencimiento=" + fvencimiento + ", hvencimiento=" + hvencimiento + ", fdevolucion=" + fdevolucion + ", hdevolucion=" + hdevolucion + ", multa=" + multa + '}';
     }
-    
+
 }

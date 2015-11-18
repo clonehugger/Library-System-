6fd5045643bc5308618e869bf3739a986c2c46c2
clonehugger <andrea.puente@udem.edu>
2015-11-17 20:24:07 -0600

    Create Prueba.java

---
+++ Prueba.java
@@ -0,0 +1,21 @@
+public class Prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Fecha fBien = new Fecha(1,1,1);
+        System.out.println(fBien);
+        Fecha fMal = new Fecha(0,0,0);
+        System.out.println(fMal);
+        Hora hBien = new Hora(13,88,71);
+        System.out.println(hBien);
+        Hora hMal = new Hora(99,99,99);
+        System.out.println(hMal);
+        
+
+// TODO code application logic here
+    }
+    
+}

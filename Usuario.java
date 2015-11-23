957250d793550410134f4884c4a4e0d440312e8c
clonehugger <andrea.puente@udem.edu>
2015-11-23 13:18:07 -0600

    Update Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -10,25 +10,23 @@ import java.io.InputStreamReader;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author clonehugger
  */
-public class Usuario extends Persona implements Serializable{
+public class Usuario extends Persona implements Serializable {
+
     private String username;
     private String password;
 
-    
+    public Usuario(Nombre nombre, String telefono, String email, String username, String password) {
 
-    
-    public Usuario(Nombre nombre, String telefono, String email, String username, String password ) {
-      
-      super(nombre, telefono, email);
-      setUsername(username);
-      setPassword(password);
-    
+        super(nombre, telefono, email);
+        setUsername(username);
+        setPassword(password);
+
     }
+
     public String getUsername() {
         return username;
     }
@@ -44,52 +42,82 @@ public class Usuario extends Persona implements Serializable{
     public void setPassword(String password) {
         this.password = password;
     }
-    
+
     @Override
     public String toString() {
-        return super.toString() + "username=" + username + ", password=" + password ;
+        return super.toString() + "username=" + username + ", password=" + password;
     }
-    
-    public boolean validarUsuario(){
+
+    public boolean validarUsuario() {
         boolean login = false;
-        File f= null;
-	FileInputStream f1=null;
-	InputStreamReader f2=null;
-	BufferedReader f3=null;		
-	String usernameActual;
-	String passwordActual;
-        boolean busqueda=false;
-      
-	try{
-		f= new File("/Users/clonehugger/Desktop/ListaUsuarios.txt");
-		f1= new FileInputStream(f);
-		f2= new InputStreamReader(f1);
-		f3= new BufferedReader(f2);
-		usernameActual=f3.readLine();
-		passwordActual=f3.readLine();		 
-                while((usernameActual!=null)&&(busqueda==false)){
-                    if(usernameActual.equals(username)){
-                        login = validarPassword(passwordActual);
-                        busqueda=true;
-                    }
-		
-		 usernameActual=f3.readLine();
-		 passwordActual=f3.readLine();
-	
+        File f = null;
+        FileInputStream f1 = null;
+        InputStreamReader f2 = null;
+        BufferedReader f3 = null;
+        String usernameActual;
+        String passwordActual;
+        boolean busqueda = false;
+
+        try {
+            f = new File("/users/clonehugger/desktop/libreria/usuarios.txt");
+            f1 = new FileInputStream(f);
+            f2 = new InputStreamReader(f1);
+            f3 = new BufferedReader(f2);
+            int cont = 0;
+            f3.readLine();
+            f3.readLine();
+            f3.readLine();
+            f3.readLine();
+            usernameActual = f3.readLine();
+            passwordActual = f3.readLine();
+            f3.readLine();
+            while ((usernameActual != null) && (busqueda == false)) {
+                if (usernameActual.equals(username)) {
+                    login = validarPassword(passwordActual);
+                    busqueda = true;
+                }
+
+                f3.readLine();
+                f3.readLine();
+                f3.readLine();
+                usernameActual = f3.readLine();
+                passwordActual = f3.readLine();
+                
+                f3.readLine();
+                if(cont ==38)
+                    f3.readLine();
+                
+                
+                
+                cont++;
+
             }
-	
-	f3.close();
-	
-	}
-	catch(IOException e){
-		System.out.println("Error de datos al iniciar sesión");
-	}
+
+            f3.close();
+
+        } catch (IOException e) {
+            System.out.println("Error de datos al iniciar sesión");
+        }
         return login;
     }
-    
-    public boolean validarPassword(String passwordActual){
+
+    public boolean validarPassword(String passwordActual) {
         return passwordActual.equals(password);
     }
-    
-    
+    public boolean finTarea(){
+        System.out.println("¿Deseas realizar otra tarea? S/N");
+        char continuar = Lectura.readChar();
+        while (true) {
+            switch (continuar) {
+                case 's':
+                case 'S':
+                    return true;
+                case 'n':
+                case 'N':
+                    return false;
+                default:
+            }
+        }
+    }
+
 }
